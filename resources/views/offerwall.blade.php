d0a50fcec6a1155132d968a2e6dfb256c58d6158
Rahul Chouhan <r.chouhan64@gmail.com>
2025-03-27 19:11:07 +0530

    new changes

--- resources/views/offerwall.blade.php
+++ resources/views/offerwall.blade.php
@@ -130,9 +130,13 @@
                         $totalPayoutGiven = round($totalPayoutGiven);
                      @endphp
                   @endif
-                  @php $totalPayoutGiven.=' '.$appDetails->currencyNameP; @endphp
+                  @php 
+                     $totalPayoutGiven = number_format($totalPayoutGiven).' '.$appDetails->currencyNameP; 
+                  @endphp
                @else 
-                  @php $totalPayoutGiven.=' '.$appDetails->currencyName; @endphp
+                  @php 
+                     $totalPayoutGiven = number_format($totalPayoutGiven).' '.$appDetails->currencyName;
+                  @endphp
                @endif
                @php 
                   $ufto = base64_encode($offer['link']);
@@ -158,14 +162,12 @@
                   <div class="cntbxsize" style="width: calc(100% - 107px); display: flex; align-items: center; justify-content: space-between;">
                      <div style="width: calc(100% - 200px);">
                         <h2 style="margin: 0 0 10px; font-weight: 600; font-size: 20px; color: {{ $offerWallTemplate->offerText }};">{{ $offer['title'] }}</h2>
-                        <div style="display:flex;gap: 14px;align-items: center;">
-                           {{-- <svg width="18" height="18" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M8 0C9.06087 0 10.0783 0.421427 10.8284 1.17157C11.5786 1.92172 12 2.93913 12 4C12 5.06087 11.5786 6.07828 10.8284 6.82843C10.0783 7.57857 9.06087 8 8 8C6.93913 8 5.92172 7.57857 5.17157 6.82843C4.42143 6.07828 4 5.06087 4 4C4 2.93913 4.42143 1.92172 5.17157 1.17157C5.92172 0.421427 6.93913 0 8 0ZM8 2C7.46957 2 6.96086 2.21071 6.58579 2.58579C6.21071 2.96086 6 3.46957 6 4C6 4.53043 6.21071 5.03914 6.58579 5.41421C6.96086 5.78929 7.46957 6 8 6C8.53043 6 9.03914 5.78929 9.41421 5.41421C9.78929 5.03914 10 4.53043 10 4C10 3.46957 9.78929 2.96086 9.41421 2.58579C9.03914 2.21071 8.53043 2 8 2ZM8 9C10.67 9 16 10.33 16 13V16H0V13C0 10.33 5.33 9 8 9ZM8 10.9C5.03 10.9 1.9 12.36 1.9 13V14.1H14.1V13C14.1 12.36 10.97 10.9 8 10.9Z" fill="{{ $offerWallTemplate->offerText }}"></path>
-                           </svg> --}}
+                        {{-- <div style="display:flex;gap: 14px;align-items: center;">
+                           
                            <div style="font-size: 18px;color:{{ $offerWallTemplate->offerText }}">
                               {{ implode(',',$offer['categories']) }}
                            </div>
-                        </div>
+                        </div> --}}
                      </div>
                      
                      <div style="min-width: 150px;">
@@ -214,17 +216,14 @@
                <div style="display: flex ; align-items: center; justify-content: space-between; width: 100%;">
                   <div style="">
                      <h2 style="margin: 0 0 10px; font-family: 'Inter'; font-weight: 600; font-size: 16px; color: {{ $offerWallTemplate->offerText }};" id="offer-title-pop">--</h2>
-                     <h3 style="margin: 0 0 20px; font-family: 'Inter'; font-weight: 400; font-size: 18px; color: {{ $offerWallTemplate->offerText }};">Offer Requirments</h3>
-                     <div style="display: flex ; gap: 10px; align-items: center; color: #757575; margin-bottom: 20px;">
-                        {{-- <svg width="18" height="18" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                           <path d="M8 0C9.06087 0 10.0783 0.421427 10.8284 1.17157C11.5786 1.92172 12 2.93913 12 4C12 5.06087 11.5786 6.07828 10.8284 6.82843C10.0783 7.57857 9.06087 8 8 8C6.93913 8 5.92172 7.57857 5.17157 6.82843C4.42143 6.07828 4 5.06087 4 4C4 2.93913 4.42143 1.92172 5.17157 1.17157C5.92172 0.421427 6.93913 0 8 0ZM8 2C7.46957 2 6.96086 2.21071 6.58579 2.58579C6.21071 2.96086 6 3.46957 6 4C6 4.53043 6.21071 5.03914 6.58579 5.41421C6.96086 5.78929 7.46957 6 8 6C8.53043 6 9.03914 5.78929 9.41421 5.41421C9.78929 5.03914 10 4.53043 10 4C10 3.46957 9.78929 2.96086 9.41421 2.58579C9.03914 2.21071 8.53043 2 8 2ZM8 9C10.67 9 16 10.33 16 13V16H0V13C0 10.33 5.33 9 8 9ZM8 10.9C5.03 10.9 1.9 12.36 1.9 13V14.1H14.1V13C14.1 12.36 10.97 10.9 8 10.9Z" fill="{{ $offerWallTemplate->offerText }}"></path>
-                        </svg> --}}
-                        <div style="font-size: 18px;color:{{ $offerWallTemplate->offerText }}" id="offer-category-pop">
+                     <h3 style="margin: 0 0 20px; font-family: 'Inter'; font-weight: 400; font-size: 16px; color: {{ $offerWallTemplate->offerText }};">Offer Requirments</h3>
+                     {{-- <div style="display: flex ; gap: 10px; align-items: center; color: #757575; margin-bottom: 20px;">
+                        <div style="font-size: 18px;font-family: 'Inter';color:{{ $offerWallTemplate->offerText }}" id="offer-category-pop">
                            --
                         </div>
-                     </div>
+                     </div> --}}
                      <div style="width:100%" class="cntbx">
-                        <p style="margin: 0; font-family: 'Inter'; font-size: 15px; color:{{ $offerWallTemplate->offerText }}" id="offer-description-pop">---</p>
+                        <p style="margin: 0; font-family: 'Inter'; font-size: 14px; color:{{ $offerWallTemplate->offerText }}" id="offer-description-pop">---</p>
                      </div>
                      <a href="#" style="display: inline-block; padding: 10px 30px; border-radius: 60px; background:{{ $offerWallTemplate->offerButtonBg }}; font-family: 'Inter';  font-size: 18px; color: {{ $offerWallTemplate->offerButtonText }}; text-decoration: none;" id="offer-price-pop">----</a>
                   </div>
@@ -344,7 +343,7 @@
              var image = $(this).attr('image')
              var category = $(this).attr('category')
 
-             $('#offer-price-pop').html(price)
+             $('#offer-price-pop').html("+ "+price)
              $('#offer-price-pop').attr('href',redirectLink)
              $('#offer-title-pop').html(title)
              $('#offer-description-pop').html(description)
